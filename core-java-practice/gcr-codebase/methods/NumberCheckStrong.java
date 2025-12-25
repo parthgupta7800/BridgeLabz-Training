391e0cd825697dd9a6f894be82c956c651923cfd
Parth Gupta <122226563+parthgupta7800@users.noreply.github.com>
2025-12-25 17:11:22 +0530

    [Parth Gupta] Add .Added check number perfect, strong, abundant and deficient

---
+++ core-java-practice/gcr-codebase/methods/NumberCheckStrong.java
@@ -0,0 +1,47 @@
+import java.util.*;
+class NumberChecker {
+    public static int sumOfProperDivisors(int number) {
+        int sum = 0;
+        for (int i = 1; i <= number / 2; i++) {
+            if (number % i == 0) {
+                sum += i;
+            }
+        }
+        return sum;
+    }
+    public static boolean isPerfectNumber(int number) {
+        return sumOfProperDivisors(number) == number;
+    }
+    public static boolean isAbundantNumber(int number) {
+        return sumOfProperDivisors(number) > number;
+    }
+    public static boolean isDeficientNumber(int number) {
+        return sumOfProperDivisors(number) < number;
+    }
+    public static int factorial(int digit) {
+        int fact = 1;
+        for (int i = 1; i <= digit; i++) {
+            fact *= i;
+        }
+        return fact;
+    }
+    public static boolean isStrongNumber(int number) {
+        int temp = number;
+        int sum = 0;
+        while (temp != 0) {
+            int digit = temp % 10;
+            sum += factorial(digit);
+            temp /= 10;
+        }
+        return sum == number;
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a number: ");
+        int number = sc.nextInt();
+        System.out.println("Is Perfect Number: " + isPerfectNumber(number));
+        System.out.println("Is Abundant Number: " + isAbundantNumber(number));
+        System.out.println("Is Deficient Number: " + isDeficientNumber(number));
+        System.out.println("Is Strong Number: " + isStrongNumber(number));
+    }
+}
