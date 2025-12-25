1d587dbd0f2a6674c9d32e55de1a4e205d91ddf0
Parth Gupta <122226563+parthgupta7800@users.noreply.github.com>
2025-12-25 17:17:26 +0530

    [Parth Gupta] Add .Added factor greatest, sum and product

---
+++ core-java-practice/gcr-codebase/methods/FactorsUtility.java
@@ -0,0 +1,60 @@
+import java.util.*;
+class FactorsUtility {
+    public static int[] findFactors(int number) {
+        int count = 0;
+        for (int i = 1; i <= number; i++) {
+            if (number % i == 0) {
+                count++;
+            }
+        }
+        int[] factors = new int[count];
+        int index = 0;
+        for (int i = 1; i <= number; i++) {
+            if (number % i == 0) {
+                factors[index++] = i;
+            }
+        }
+        return factors;
+    }
+    public static int findGreatestFactor(int[] factors) {
+        int greatest = Integer.MIN_VALUE;
+        for (int factor : factors) {
+            if (factor > greatest) {
+                greatest = factor;
+            }
+        }
+        return greatest;
+    }
+    public static int sumOfFactors(int[] factors) {
+        int sum = 0;
+        for (int factor : factors) {
+            sum += factor;
+        }
+        return sum;
+    }
+    public static long productOfFactors(int[] factors) {
+        long product = 1;
+        for (int factor : factors) {
+            product *= factor;
+        }
+        return product;
+    }
+    public static double productOfCubeOfFactors(int[] factors) {
+        double product = 1;
+        for (int factor : factors) {
+            product *= Math.pow(factor, 3);
+        }
+        return product;
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a number: ");
+        int number = sc.nextInt();
+        int[] factors = findFactors(number);
+        System.out.println("Factors Array: " + Arrays.toString(factors));
+        System.out.println("Greatest Factor: " + findGreatestFactor(factors));
+        System.out.println("Sum of Factors: " + sumOfFactors(factors));
+        System.out.println("Product of Factors: " + productOfFactors(factors));
+        System.out.println("Product of Cube of Factors: " + productOfCubeOfFactors(factors));
+    }
+}
