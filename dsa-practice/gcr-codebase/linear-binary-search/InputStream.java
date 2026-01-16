4ff7e91583ab1979a016116a8bc8c08f1e3d2d1c
Parth Gupta <122226563+parthgupta7800@users.noreply.github.com>
2026-01-16 16:33:27 +0530

    [Parth Gupta] Add .Added Byte Stream to Character Stream

---
+++ dsa-practice/gcr-codebase/linear-binary-search/InputStream.java
@@ -0,0 +1,25 @@
+import java.io.*;
+public class InputStream {
+    public static void main(String[] args) {
+        BufferedReader line = null;
+        try {
+            FileInputStream file = new FileInputStream("sample.txt");
+            InputStreamReader isr=new InputStreamReader(file,"UTF-8");
+            int ch;
+            line = new BufferedReader(isr);
+            while((ch=line.read())!=-1){
+                System.out.println(ch);
+            }
+        } catch (IOException e) {
+            System.out.println("An error occurred: " + e.getMessage());
+        } finally {
+            try {
+                if (line != null) {
+                    line.close();
+                }
+            } catch (IOException e) {
+                System.out.println("An error occurred while closing the reader: " + e.getMessage());
+            }
+        }
+    }
+}
