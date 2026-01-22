ec30c93e9bcc0aa3aa123b1bd0fb1df9ba45f4d8
Parth Gupta <122226563+parthgupta7800@users.noreply.github.com>
2026-01-22 10:41:41 +0530

    [Parth Gupta] Add .Added EduResults – Rank Sheet Generator

---
+++ dsa-practice/scenario-based/EduResult.java
@@ -0,0 +1,63 @@
+import java.util.*;
+public class EduResult {
+    public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+        System.out.print("Enter total number of students: ");
+        int n=sc.nextInt();
+        int[] scores=new int[n];
+        for(int i=0;i<n;i++){
+            System.out.print("Enter score of student "+(i+1)+": ");
+            scores[i]=sc.nextInt();
+        }
+        MergeSorter.mergeSort(scores,0,n-1);
+        System.out.println("\nFinal State-Wise Rank List (Ascending Scores):");
+        for(int i=0;i<n;i++){
+            System.out.println("Rank "+(i+1)+": "+scores[i]);
+        }
+    }
+}
+class MergeSorter {
+    public static void merge(int[] arr,int left,int mid,int right){
+        int n1=mid-left+1;
+        int n2=right-mid;
+        int[] L=new int[n1];
+        int[] R=new int[n2];
+        for(int i=0;i<n1;i++){
+            L[i]=arr[left+i];
+        }
+        for(int j=0;j<n2;j++){
+            R[j]=arr[mid+1+j];
+        }
+        int i=0;
+        int j=0;
+        int k=left;
+        while(i<n1 && j<n2){
+            if(L[i]<=R[j]){
+                arr[k]=L[i];
+                i++;
+            }else{
+                arr[k]=R[j];
+                j++;
+            }
+            k++;
+        }
+        while(i<n1){
+            arr[k]=L[i];
+            i++;
+            k++;
+        }
+        while(j<n2){
+            arr[k]=R[j];
+            j++;
+            k++;
+        }
+    }
+    public static void mergeSort(int[] arr,int left,int right){
+        if(left<right){
+            int mid=(left+right)/2;
+            mergeSort(arr,left,mid);
+            mergeSort(arr,mid+1,right);
+            merge(arr,left,mid,right);
+        }
+    }
+}
