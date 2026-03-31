14a77d305c459e98bdbb3e03d0ef8d875d857722
Parth Gupta <122226563+parthgupta7800@users.noreply.github.com>
2026-03-31 15:02:13 +0530

    Revert "Merge branch 'main' into core-java-practice"
    This reverts commit 560b324477627c217c3f4e13c06d5998d654c1c8, reversing
    changes made to 4fd8da92bebce2644792b025cfff747601c65acb.

--- dsa-practice/leet-code-codebase/SubstringPattern.java
+++
@@ -1,7 +0,0 @@
-class SubstringPattern {
-    public boolean repeatedSubstringPattern(String s){
-        String str=s+s;
-        String str2=str.substring(1,str.length()-1);
-        return str2.contains(s);
-    }
-}
